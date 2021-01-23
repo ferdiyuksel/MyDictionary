a34258168e4268b107b33754b858ce96152a69f1
Ferdi Yüksel <ferdiyuksel@yahoo.com>
2021-01-23 23:02:38 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<Dizi>
+    {
+        Dizi[] items;
+        public MyList()
+        {
+            items = new Dizi[0];
+        }
+        public void Add(Dizi item)
+        {
+            Dizi[] tempArray = items;
+            items = new Dizi[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+        public int Lenght
+        {
+            get { return items.Length; }
+        }
+
+        public Dizi[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler = new MyList<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("Bursa");
+            sehirler.Add("Aksaray");
+
+            Console.WriteLine(sehirler.Lenght);
+        }
+    }
+}
